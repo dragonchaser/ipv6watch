407aee0c48b74afbc46e70d747c5c00edd136349
Daniel Niedermeyer <daniel.niedermeyer@stud.hs-regensburg.de>
2013-10-24 22:39:00 +0200

    cleanup

--- client/pydemo/client.py
+++ client/pydemo/client.py
@@ -9,32 +9,60 @@ __author__ = 'daniel'
 import re
 import paramiko
 
-client = paramiko.SSHClient()
-client.load_system_host_keys()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-print 'Connecting...'
-client.connect('194.95.109.129', username='tracker', password='quetschi12', timeout=20, allow_agent=False, look_for_keys=False)
-channel = client.invoke_shell()
-stdin = channel.makefile('wb')
-stdout = channel.makefile('rb')
 
-# terminal length 0: set the number of lines of output to display on the terminal screen for the current session
-# show ipv6 neigh: display IPv6 neighbor discovery cache
-# exit: close the shell
-stdin.write('''
-terminal length 0
-show ipv6 neigh
-exit
-''')
-lines = stdout.read().splitlines()
-client.close()
-print 'Finished'
+class Router:
+    def __init__(self, ip, username, password):
+        self.ip = ip
+        self.username = username
+        self.password = password
 
-p = re.compile(r'^(?P<ip>[\da-f:]+)[ \t]+(?P<ttl>[\d\-]+)[ \t]+(?P<mac>[\da-f\.]+)[ \t]+(?P<state>(STALE|REACH))[ \t]+(?P<if>[a-zA-Z\d]+)\Z', re.I)
-for line in lines:
-    line = line.strip()
-    result = p.match(line)
-    if result:
-        print('ip:' + result.group('ip') + ' mac:' + result.group('mac')+ ' state:' + result.group('state') +' interface:' + result.group('if'))
-    else:
-        print('no match: ' + line)
+    def getneighbors(self):
+        sshClient = paramiko.SSHClient()
+        sshClient.load_system_host_keys()
+        sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        sshClient.connect(self.ip, username=self.username, password=self.password, timeout=20, allow_agent=False,
+                          look_for_keys=False)
+        channel = sshClient.invoke_shell()
+        stdin = channel.makefile('wb')
+        stdout = channel.makefile('rb')
+
+        # terminal length 0: set the number of lines of output to display on the terminal screen for the current session
+        # show ipv6 neigh: display IPv6 neighbor discovery cache
+        # exit: close the shell
+        stdin.write('''
+        terminal length 0
+        show ipv6 neigh
+        exit
+        ''')
+        lines = stdout.read().splitlines()
+        sshClient.close()
+        p = re.compile(
+            r'^(?P<ip>[\da-f:]+)[ \t]+(?P<age>[\d\-]+)[ \t]+(?P<mac>[\da-f\.]+)[ \t]+(?P<state>(STALE|REACH))[ \t]+(?P<if>[a-zA-Z\d]+)\Z', re.I)
+        clientList = []
+        for line in lines:
+            line = line.strip()
+            result = p.match(line)
+            if result:
+                clientList.append(
+                    Client(result.group('ip'), result.group('mac'), result.group('state'), result.group('age'), self.ip,
+                           result.group('if')))
+        return clientList
+
+
+class Client:
+    def __init__(self, ip, mac, state, age, router, interface):
+        self.ip = ip
+        self.mac = mac
+        self.state = state
+        self.age = age
+        self.router = router
+        self.interface = interface
+
+
+clientList = []
+routerList = [Router('194.95.109.129', 'tracker', 'quetschi12')]
+for r in routerList:
+    clientList.extend(r.getneighbors())
+
+for c in clientList:
+    print c.ip + ' - ' + c.mac + ' - ' + c.router
