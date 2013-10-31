fd868577612b326d8dc19ec610c2a2565a435f5c
Daniel Niedermeyer <daniel.niedermeyer@stud.hs-regensburg.de>
2013-10-31 11:50:48 +0100

    implemented database access using sqlalchemy moved settings to config.cfg

--- client/pydemo/client.py
+++ client/pydemo/client.py
@@ -1,60 +1,60 @@
-import time
-
-__author__ = 'daniel'
-#
-# a first demo client, quick&dirty, no error handling
-# python 2.7.5
-# requires paramiko
-# https://github.com/paramiko/paramiko
-#
-
+import datetime
 import re
 import paramiko
 import threading
+import ConfigParser
+from sqlalchemy import create_engine, select, sql, Table, exc, func, MetaData
 
 
 class Router(threading.Thread):
-    def __init__(self, ip, username, password):
+    def __init__(self, id, name, ip, username, password):
+        threading.Thread.__init__(self)
+        self.id = id
+        self.name = name
         self.ip = ip
         self.username = username
         self.password = password
         self.neighbors = []
-        threading.Thread.__init__(self)
 
     def run(self):
-        print 'Thread started: '+self.ip
-        sshclient = paramiko.SSHClient()
-        sshclient.load_system_host_keys()
-        sshclient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        sshclient.connect(self.ip, username=self.username, password=self.password, timeout=20, allow_agent=False,
-                          look_for_keys=False)
-        channel = sshclient.invoke_shell()
-        stdin = channel.makefile('wb')
-        stdout = channel.makefile('rb')
+        try:
+            print 'Thread %i started' % self.id
+            sshclient = paramiko.SSHClient()
+            sshclient.load_system_host_keys()
+            sshclient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            sshclient.connect(self.ip, username=self.username, password=self.password, timeout=20, allow_agent=False,
+                              look_for_keys=False)
+            channel = sshclient.invoke_shell()
+            stdin = channel.makefile('wb')
+            stdout = channel.makefile('rb')
 
-        # terminal length 0: set the number of lines of output to display on the terminal screen for the current session
-        # show ipv6 neigh: display IPv6 neighbor discovery cache
-        # exit: close the shell
-        stdin.write('''
-        terminal length 0
-        show ipv6 neigh
-        exit
-        ''')
-        lines = stdout.read().splitlines()
-        timestamp = time.time()
-        sshclient.close()
-        p = re.compile(
-            r'^(?P<ip>[\da-f:]+)[ \t]+(?P<age>[\d\-]+)[ \t]+(?P<mac>[\da-f\.]+)[ \t]+(?P<state>(STALE|REACH))[ \t]+(?P<if>[a-zA-Z\d]+)\Z', re.I)
-        clients = []
-        for line in lines:
-            line = line.strip()
-            result = p.match(line)
-            if result:
-                clients.append(
-                    Client(result.group('ip'), result.group('mac'), result.group('state'), result.group('age'), self.ip,
-                           result.group('if'), timestamp))
-        print 'Thread finished: '+self.ip
-        self.neighbors = clients
+            # terminal length 0: set the number of lines of output to display on the terminal screen for the current session
+            # show ipv6 neigh: display IPv6 neighbor discovery cache
+            # exit: close the shell
+            stdin.write('''
+            terminal length 0
+            show ipv6 neigh
+            exit
+            ''')
+            lines = stdout.read().splitlines()
+            timestamp = datetime.datetime.now()
+            sshclient.close()
+            p = re.compile(
+                r'^(?P<ip>[\da-f:]+)[ \t]+(?P<age>[\d\-]+)[ \t]+(?P<mac>[\da-f\.]+)[ \t]+(?P<state>(STALE|REACH))[ \t]+(?P<if>[a-zA-Z\d]+)\Z',
+                re.I)
+            clients = []
+            for line in lines:
+                line = line.strip()
+                result = p.match(line)
+                if result:
+                    clients.append(
+                        Client(result.group('ip'), result.group('mac').replace('.', ''), result.group('state'),
+                               result.group('age'), self.id,
+                               result.group('if'), timestamp - datetime.timedelta(minutes=int(result.group('age')))))
+            print 'Thread finished: ' + self.ip
+            self.neighbors = clients
+        except Exception, e:
+            print 'Thread %i failed: %s' % (self.id, str(e))
 
 
 class Client:
@@ -69,14 +69,29 @@ class Client:
 
 
 print 'Main Thread started'
-threads = []
-results = []
-routerList = [Router('194.95.109.129', 'tracker', 'quetschi12')]
+print 'Parsing Config'
+config = ConfigParser.ConfigParser()
+config.read('config.cfg')
 
-# start a thread for every router
-for r in routerList:
+engine = create_engine(
+    config.get('Database', 'type') + '://' + config.get('Database', 'user') + ':' + config.get('Database', 'pass') + '@' + config.get(
+        'Database', 'host') + '/' + config.get('Database', 'database'), echo=True)
+conn = engine.connect()
+metadata = MetaData(engine)
+tbl_router = Table(u'ipv6_routerdata', metadata, autoload=True)
+tbl_logentry = Table(u'ipv6_logentry', metadata, autoload=True)
+tbl_timelog = Table(u'ipv6_timelog', metadata, autoload=True)
+res = select([tbl_router.c.id, tbl_router.c.routerName, tbl_router.c.ipv4Address, tbl_router.c.username,
+              tbl_router.c.password]).where(tbl_router.c.active == 1).execute()
+
+threads = []
+routerList = []
+for row in res:
+    r = Router(row['id'], row['routerName'], row['ipv4Address'], row['username'], row['password'])
     threads.append(r)
     r.start()
+
+results = []
 
 # wait until all threads are finished
 for t in threads:
@@ -84,5 +99,16 @@ for t in threads:
     results.extend(t.neighbors)
 
 for c in results:
-    print c.ip + ' - ' + c.mac + ' - ' + c.router
+    try:
+        res = tbl_logentry.insert().values(
+            {'ipv6Address': c.ip, 'macAddress': c.mac, 'date_added': func.now(), 'RouterData': c.router}).execute()
+        logid = res.lastrowid
+    except exc.IntegrityError:
+        res = select([tbl_logentry.c.id]).where(
+            sql.and_(tbl_logentry.c.ipv6Address == c.ip, tbl_logentry.c.macAddress == c.mac)).execute()
+        logid = res.fetchone()[0]
+    finally:
+        res = tbl_timelog.insert().values({'lastseen': c.seen, 'logentry': logid}).execute()
+
+conn.close()
 print 'Main Thread finished'
