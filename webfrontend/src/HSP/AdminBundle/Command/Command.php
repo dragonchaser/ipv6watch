303312a04ed2df7ac687a2af19a8495bef6216a2
Christian Richter <christian@boltares.de>
2013-11-06 12:47:27 +0100

    revs #3 * added usercreate script to app console

---
+++ webfrontend/src/HSP/AdminBundle/Command/Command.php
@@ -0,0 +1,43 @@
+<?php
+namespace HSP\AdminBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Command extends ContainerAwareCommand
+{
+	protected function configure()
+	{
+		$this
+			->setName('hsp:createuser')
+			->setDescription('create initial user');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		//$this->container = $this->getApplication()->getKernel()->getContainer();
+		$output->writeln("Creating initial user");
+		$dialog = $this->getHelperSet()->get('dialog');
+		$username = $dialog->ask($output, "Pleas enter a username: ");
+		$password = $dialog->askHiddenResponse($output, "Please enter a password for '$username': ", false);
+		$passwordval = $dialog->askHiddenResponse($output, "Please validate password for '$username': ", false);
+		if ($password !== $passwordval) $output->writeln("Password mismatch, try again"); else {
+			$output->writeln("Creating user '$username'");
+			$userManager = $this->getContainer()->get('fos_user.user_manager');
+			$newUser = $userManager->createUser();
+			$newUser->setUsername($username);
+			$newUser->setUsernameCanonical($username);
+			$encoder = $this->getContainer()->get('security.encoder_factory')->getEncoder($newUser);
+			$newPassword = $encoder->encodePassword($password, $newUser->getSalt());
+			$newUser->setPassword($newPassword);
+			$newUser->setEmail("admin@yourhost.com");
+			$newUser->addRole("ROLE_ADMIN");
+			$newUser->setEnabled(true);
+			$this->getContainer()->get('doctrine')->getManager()->persist($newUser);
+			$this->getContainer()->get('doctrine')->getManager()->flush();
+		}
+	}
+}
--- webfrontend/src/HSP/AdminBundle/Controller/DefaultController.php
+++ webfrontend/src/HSP/AdminBundle/Controller/DefaultController.php
@@ -7,44 +7,44 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('HSPAdminBundle:Default:index.html.twig', array(
-            'name' => "admin"
-        ));
-    }
+	public function indexAction()
+	{
+		return $this->render('HSPAdminBundle:Default:index.html.twig', array(
+			'name' => "admin"
+		));
+	}
 
-    public function userHandlingAction()
-    {
-        $userManager = $this->get('fos_user.user_manager');
-        $users = $userManager->findUsers();
-        return $this->render('HSPAdminBundle:Default:userlist.html.twig', array(
-            'name' => "userHandler",
-            'users' => $users
-        ));
-    }
+	public function userHandlingAction()
+	{
+		$userManager = $this->get('fos_user.user_manager');
+		$users = $userManager->findUsers();
+		return $this->render('HSPAdminBundle:Default:userlist.html.twig', array(
+			'name' => "userHandler",
+			'users' => $users
+		));
+	}
 
-    public function userEditHandlingAction($username)
-    {
-        return $this->render('HSPAdminBundle:Default:useredit.html.twig', array(
-            'username' => $username
-        ));
-    }
+	public function userEditHandlingAction($username)
+	{
+		return $this->render('HSPAdminBundle:Default:useredit.html.twig', array(
+			'username' => $username
+		));
+	}
 
-    public function userDeleteConfirmAction($username)
-    {
-        return $this->render('HSPAdminBundle:Default:userdeleteconfirm.html.twig', array(
-            'username' => $username
-        ));
-    }
+	public function userDeleteConfirmAction($username)
+	{
+		return $this->render('HSPAdminBundle:Default:userdeleteconfirm.html.twig', array(
+			'username' => $username
+		));
+	}
 
-    public function userDeleteAction($username)
-    {
-        $userManager = $this->get('fos_user.user_manager');
-        $user = $userManager->findUserBy(array('username' => $username));
-        $userManager->deleteUser($user);
-        $this->get('session')->getFlashBag()->set('notice', $user . ' has been deleted!');
+	public function userDeleteAction($username)
+	{
+		$userManager = $this->get('fos_user.user_manager');
+		$user = $userManager->findUserBy(array('username' => $username));
+		$userManager->deleteUser($user);
+		$this->get('session')->getFlashBag()->set('notice', $user . ' has been deleted!');
 
-        return new RedirectResponse($this->generateUrl('users'));
-    }
+		return new RedirectResponse($this->generateUrl('users'));
+	}
 }
