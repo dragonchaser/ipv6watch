17b75fbd0fdf9373992e77c9cd70665ddb4e45fd
Christian Richter <christian@boltares.de>
2013-11-24 21:21:06 +0100

    - improved user management (revs #1)

--- webfrontend/src/HSP/AdminBundle/Menu/Builder.php
+++ webfrontend/src/HSP/AdminBundle/Menu/Builder.php
@@ -6,23 +6,21 @@ use Symfony\Component\DependencyInjection\ContainerAware;
 
 class Builder extends ContainerAware
 {
-	public function mainMenu(FactoryInterface $factory, array $options)
-	{
-		$menu = $factory->createItem('root');
-		$menu->addChild('Home', array('route' => 'hsp_page_homepage'));
-		$menu->addChild('Admin', array('route' => 'hsp_admin_link'));
+  public function mainMenu(FactoryInterface $factory, array $options)
+  {
+    $menu = $factory->createItem('root');
+    $menu->addChild('Home', array('route' => 'hsp_page_homepage'));
+    $menu->addChild('Admin', array('route' => 'hsp_admin_link'));
 
-		$menu->addChild('User', array('route' => 'hsp_admin_user_handling'))
-			//->addChild('Add User', array('route' => 'fos_user_registration_register'))->getParent()
-			->addChild('Add User', array('route' => 'hsp_admin_user_add'))->getParent()
-			->addChild('Change Password', array('route' => 'fos_user_change_password'))->getParent()
-			->addChild('Logout', array('route' => 'fos_user_security_logout'));
+    $menu->addChild('User', array('route' => 'hsp_admin_user_handling'))
+      ->addChild('Add User', array('route' => 'hsp_admin_user_add'))->getParent()
+      ->addChild('Logout', array('route' => 'fos_user_security_logout'));
 
-		$menu->addChild('Leases', array('route' => 'hsp_admin_leaselist'))
-			->addChild('Exports', array('route' => 'hsp_admin_lease_exports'));
+    $menu->addChild('Leases', array('route' => 'hsp_admin_leaselist'))
+      ->addChild('Exports', array('route' => 'hsp_admin_lease_exports'));
 
-		$menu->addChild('Router', array('route' => 'hsp_admin_router_handling'))
-			->addChild('Add Router', array('route' => 'hsp_admin_router_add'));
-		return $menu;
-	}
+    $menu->addChild('Router', array('route' => 'hsp_admin_router_handling'))
+      ->addChild('Add Router', array('route' => 'hsp_admin_router_add'));
+    return $menu;
+  }
 }
