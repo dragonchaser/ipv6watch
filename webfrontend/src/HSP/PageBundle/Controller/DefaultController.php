f1208d45603fa76f8832a76ffd6e8e041bf8c27f
Christian Richter <christian@boltares.de>
2014-01-14 12:45:07 +0100

    * revs #1 ** added statistics to front page

--- webfrontend/src/HSP/PageBundle/Controller/DefaultController.php
+++ webfrontend/src/HSP/PageBundle/Controller/DefaultController.php
@@ -8,6 +8,18 @@ class DefaultController extends Controller
 {
   public function indexAction()
   {
-    return $this->render('HSPPageBundle:Default:index.html.twig', array('name' => "default page"));
+    $doctrine = $this->getDoctrine();
+    $ipCount = count($doctrine->getRepository('HSPPageBundle:IPV6IpAddress')->findAll());
+    $logCount = count($doctrine->getRepository('HSPPageBundle:IPV6TimeLog')->findAll());
+    $routerCount = count($doctrine->getRepository('HSPPageBundle:IPV6Router')->findAll());
+    $runCount = count($doctrine->getRepository('HSPPageBundle:IPV6CronRuns')->findAll());
+    return $this->render('HSPPageBundle:Default:index.html.twig',
+      array(
+        'ipCount' => $ipCount,
+        'logCount' => $logCount,
+        'routerCount' => $routerCount,
+        'runCount' => $runCount
+      )
+    );
   }
 }
